a0e43b5614d7463fbaada7d97e5e0844189e8683
Nataliia_Pachkovska <Nataliia_Pachkovska@epam.com>
2019-02-27 17:59:11 +0200

    list of courses

--- my-app/src/components/login/login.js
+++ my-app/src/components/login/login.js
@@ -9,32 +9,40 @@ class Login extends Component {
       this.state = {
           login: '',
           password: '',
-          loginValid: false,
-          passwordValid: false,
+          loginValid: true,
+          passwordValid: true,
           formValid: false,
           redirectToReferrer: false
         };
       this.handleUserLogin = this.handleUserLogin.bind(this);
       this.handleUserPassword = this.handleUserPassword.bind(this);
       this.submitLoginForm = this.submitLoginForm.bind(this);
-    }
+    };
 
     handleUserLogin = ({ target: { value } }) => {
-
         this.setState({
             login: value
-        },
-        () => { this.validateLoginField(value) });
+        });
     };
 
     handleUserPassword = ({ target: { value } }) => {
         this.setState({
             password: value
+        });
+    };
+
+    renderUserError = ({ target: { value } }) => {
+        this.setState({
+            loginValid: false
+        },
+        () => { this.validateLoginField(value) });
+    };
+    renderPasswordError = ({ target: { value } }) => {
+        this.setState({
+            passwordValid: false
         },
         () => { this.validatePasswordField(value) });
     };
-
-
 
     validateLoginField(value) {
         let regexp = /[a-zA-Z]+/g;
@@ -43,7 +51,7 @@ class Login extends Component {
         this.setState({
             loginValid: loginValid
         }, this.validateForm);
-    }
+    };
 
     validatePasswordField(value) {
         let regexp = /^\w+$/g;
@@ -52,18 +60,18 @@ class Login extends Component {
         this.setState({
             passwordValid: passwordValid
         }, this.validateForm);
-    }
+    };
 
     validateForm() {
         this.setState({formValid: this.state.loginValid && this.state.passwordValid});
-    }
+    };
+
     submitLoginForm (e) {
         e.preventDefault();
         this.setState({
             redirectToReferrer: true
         })
-    }
-
+    };
 
     render () {
         const redirectToReferrer = this.state.redirectToReferrer;
@@ -75,14 +83,16 @@ class Login extends Component {
                   <div className="formContent">
                     <Form className={`form-wrapper`} onSubmit={this.submitLoginForm}>
                         <div className="error">wrong login or password</div>
-                      <Form.Group controlId="formBasicLogin">
+                      <Form.Group controlId="formBasicLogin" className={`${this.state.loginValid ? '' : 'has-error'}`}>
                         <Form.Label>Login</Form.Label>
-                        <Form.Control type="text" name="login" placeholder="Enter login" className={`second input ${this.state.loginValid ? '' : 'has-error'}`} value={this.state.login} onChange={this.handleUserLogin}/>
+                        <Form.Control type="text" name="login" placeholder="Enter login" value={this.state.login} onChange={this.handleUserLogin} onBlur={this.renderUserError}/>
+                        <Form.Text>* enter</Form.Text>
                       </Form.Group>
 
-                      <Form.Group controlId="formBasicPassword">
+                      <Form.Group controlId="formBasicPassword" className={`${this.state.passwordValid ? '' : 'has-error'}`}>
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" name="password" placeholder="Password" className={`third input ${this.state.passwordValid ? '' : 'has-error'}`} value={this.state.password} onChange={this.handleUserPassword}/>
+                        <Form.Control type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleUserPassword} onBlur={this.renderPasswordError}/>
+                        <Form.Text>* enter</Form.Text>
                       </Form.Group>
                       <Button variant="primary" type="submit"  className="fourth btn btn-primary" disabled={!this.state.formValid}>
                         Login
